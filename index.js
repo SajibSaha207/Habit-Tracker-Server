368dee1e4e347a0ac9123e4e439da9c225112551
SajibSaha207 <sajibsaha.207@gmail.com>
2026-05-14 00:14:35 +0600

    added env file and final setup done

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const cors = require('cors');
-// require('dotenv').config()
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+}));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-
-app.use(cors());
-app.use(express.json())
-
-//
-//
-
-
-const uri = "mongodb+srv://habit_tracker:Tq1p9uRVOF09bAH3@my-app-database.mqufenv.mongodb.net/?appName=My-app-database";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@my-app-database.mqufenv.mongodb.net/?appName=My-app-database`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,122 +22,95 @@ const client = new MongoClient(uri, {
   }
 });
 
-app.get('/', (req, res)=>{
-    res.send('Smart server is running')
+app.get('/', (req, res) => {
+  res.send('Smart server is running')
 })
 
-async function run(){
-    try{
-await client.connect();
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB connected!");
 
-const db = client.db('habit_tracker');
-const habitCollection = db.collection('habits_collection');
-const usersCollection = db.collection('users');
+    const db = client.db('habit_tracker');
+    const habitCollection = db.collection('habits_collection');
+    const usersCollection = db.collection('users');
 
+    // Users
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
 
-app.get('/users', async(req, res)=>{
-    const cursor = usersCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-})
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    })
 
+    // Habits
+    app.get('/habits_collection', async (req, res) => {
+      const sort = req.query.sort;
+      const cursor = sort === 'latest'
+        ? habitCollection.find().sort({ createAt: -1 })
+        : habitCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
+    app.get('/habits_collection/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await habitCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
 
-app.post('/users', async(req, res)=>{
-    const newUser = req.body;
-    console.log('user info', newUser)
-    const result = await usersCollection.insertOne(newUser);
-    console.log(result);
-    res.send(result);
+    app.get('/habit_collection/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await habitCollection.find({ creatorEmail: email }).toArray();
+      res.send(result);
+    })
 
-})
+    app.post('/habits_collection', async (req, res) => {
+      const newHabit = req.body;
+      const result = await habitCollection.insertOne(newHabit);
+      res.send(result);
+    })
 
-//HABIT RELATED API POST
-
-app.patch('/habits_collection/:id', async(req, res)=>{
-    const id = req.params.id;
-    const updateData = req.body;
-    const result = await habitCollection.updateOne({_id: new ObjectId(id)}, { $set: updateData});
-    res.send(result);
-});
-
-app.delete('/habits_collection/:id', async(req, res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await habitCollection.deleteOne(query)
-    res.send(result);
-})
-
-app.get('/habits_collection/:id', async(req, res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id) };
-    const result = await habitCollection.findOne(query);
-    res.send(result);
-})
-
-
-//  Single habit
-app.get('/habits_collection/detail/:id', async(req, res) => {
-    const id = req.params.id;
-    const result = await habitCollection.findOne({ _id: new ObjectId(id) });
-    res.send(result);
-})
-
-// Mark Complete 
-app.patch('/habits_collection/complete/:id', async(req, res) => {
-    const id = req.params.id;
-    const { date } = req.body;
-    const result = await habitCollection.updateOne(
+    app.patch('/habits_collection/complete/:id', async (req, res) => {
+      const id = req.params.id;
+      const { date } = req.body;
+      const result = await habitCollection.updateOne(
         { _id: new ObjectId(id) },
-        { $addToSet: { completionHistory: date } } 
-    );
-    res.send(result);
-})
+        { $addToSet: { completionHistory: date } }
+      );
+      res.send(result);
+    })
 
+    app.patch('/habits_collection/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const result = await habitCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      );
+      res.send(result);
+    })
 
-app.get('/habits_collection', async(req, res)=>{
-    const cursor = habitCollection.find().sort({ createAt: -1 })
-    const result = await cursor.toArray();
-    res.send(result)
-})
-
-
-app.get('/habits_collection', async(req, res)=>{
-    const cursor = habitCollection.find() ;
-    const result = await cursor.toArray();
-    res.send(result)
-})
-
-
-
-app.get('/habit_collection/:email', async(req, res)=>{
-    const email = req.params.email;
-    const result = await habitCollection.find({ creatorEmail: email}).toArray();
-    res.send(result)
-})
-
-
-app.post('/habits_collection', async(req, res)=>{
-    const newHabit = req.body;
-    console.log('habit info', newHabit)
-    const result = await habitCollection.insertOne(newHabit);
-    console.log(result)
-    res.send(result)
-})
-
+    app.delete('/habits_collection/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await habitCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally{
+    console.log("Pinged successfully!");
 
-    }
-
+  } finally {}
 }
 
 run().catch(console.dir)
 
-
-app.listen(port, ()=>{
-    console.log(`Smart server is running on port: ${port}`)
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
 })
+
+module.exports = app;
